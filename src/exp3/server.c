12070fdfa7a74f96c5a872a079e751e320d3554f
Luís Raposo <117324881+LuisRapos0@users.noreply.github.com>
2026-04-28 18:24:19 -0300

    experimento 3

--- src/exp3/server.c
+++ src/exp3/server.c
@@ -8,15 +8,13 @@
 #include <string.h>   
 
 #define SERVER_PORT 54321
-#define MAX_PENDING 5
 #define MAX_LINE    256
 
 int main(int argc, char **argv)
 {
     struct sockaddr_in sin;
     char buf[MAX_LINE];
-    int len = sizeof(sin);
-    int s, new_s;
+    int s;
 
     /* build address data structure */
     bzero((char *)&sin, sizeof(sin));
@@ -25,24 +23,20 @@ int main(int argc, char **argv)
     sin.sin_port = htons(SERVER_PORT);
 
     /* setup passive open */
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("simplex-talk: socket");
         exit(1);
     }
+
+    int yes = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
     if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
         perror("simplex-talk: bind");
         exit(1);
     }
-    listen(s, MAX_PENDING);
 
-    /* wait for connection, then receive and print text */
-    while(1) {
-        if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
-            perror("simplex-talk: accept");
-            exit(1);
-        }
-        while (len = recv(new_s, buf, sizeof(buf), 0))
-            fputs(buf, stdout);
-        close(new_s);
-    }
+    /* receive and print datagrams */
+    while (recv(s, buf, sizeof(buf), 0))
+        fputs(buf, stdout);
 }
